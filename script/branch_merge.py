c89f5ee9d6164d4bbf677789c2bba31361567caa
HsiaoTing Chen <fatfattim@gmail.com>
2023-11-02 12:09:24 +0800

    Nice o

--- script/branch_merge.py
+++ script/branch_merge.py
@@ -1,16 +1,42 @@
-import sys
-import os
+import subprocess
 
-def execute(command: str, read_result: bool):
-    proc = os.popen(command)
-    if read_result:
-        output = proc.read()
-        print(output, flush=True)
+def execute_git_command(command):
+    try:
+        result = subprocess.run(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,  # Raise a CalledProcessError for non-zero exit codes.
+        )
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        # Handle Git command error here
+        error_message = e.stderr
+        return error_message
 
 def mergeDevIntoMaster():
-    execute("git fetch -p", read_result=True)
-    execute("git checkout -b main origin/main", read_result=True)
-    execute("git pull origin dev --no-rebase --no-edit", read_result=True)
+    # Example Git commands
+    git_commands = [
+        "git fetch -p",
+        "git checkout -b main origin/main",
+        "git pull origin dev --no-rebase --no-edit",
+    ]
+
+    for command in git_commands:
+        try:
+            output = execute_git_command(command)
+            print(output)
+            if "fatal:" in output.lower() or "merge conflict" in output.lower():
+                print("Error encountered.")
+                exit(1)
+        except subprocess.CalledProcessError as e:
+            # Handle Git command error here
+            print(f"Error executing command: {command}")
+            print(f"Error message: {e.stderr}")
+            exit(1)
+
     print("Done")
 
 if __name__ == "__main__":
