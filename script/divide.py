e7cee949c1dc8baf6413a0b33165f3f2a5f35309
HsiaoTing Chen <fatfattim@gmail.com>
2023-11-02 11:09:06 +0800

    try

--- script/divide.py
+++ script/divide.py
@@ -1,21 +1,21 @@
+import sys
+
 def divide(a, b):
     try:
         result = a / b
         return result
     except ZeroDivisionError:
-        return "Failed: Division by zero"
+        exit(1)
     except Exception as e:
-        return f"Failed: {str(e)}"
+        exit(2)
 
-# Successful division
-a = 10
-b = 2
-result = divide(a, b)
-print(f"Result of {a} / {b} is {result}")
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python divide.py <a> <b>")
+        sys.exit(1)
 
-# Attempting division by zero
-a = 5
-b = 0
-result = divide(a, b)
-print(f"Result of {a} / {b} is {result}")
+    a = float(sys.argv[1])
+    b = float(sys.argv[2])
 
+    result = divide(a, b)
+    print(f"Result of {a} / {b} is {result}")
